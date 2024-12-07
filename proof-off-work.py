5988ea03e3deadafd127843ba1027aeabcc4bad5
Rizwan Fairuz Mamduh <rizwanfairuzmamduh29@gmail.com>
2024-12-08 04:14:23 +0700

    completed proof-of-work

--- proof-off-work.py
+++ proof-off-work.py
@@ -12,10 +12,11 @@ from flask.json import jsonify
 import requests
 from urllib.parse import urlparse
 
+# === Proof Of Work ===
 class Blockchain(object):
     difficulty_target = "0000"
     def hash_block(self, block):
-        block_encoded = json.dumps(block, sort_keys=True).encode
+        block_encoded = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_encoded).hexdigest()
     def __init__(self):
         self.chain = []
@@ -26,7 +27,7 @@ class Blockchain(object):
             nonce = self.proof_of_work(0, genesis_hash, [])
         )
     
-    def proof_of_work (self, index, hash_of_previous_block, transactions, nonce):
+    def proof_of_work (self, index, hash_of_previous_block, transactions):
         nonce = 0
         while self.valid_proof (index, hash_of_previous_block, transactions, nonce) is False:
             nonce += 1
@@ -35,3 +36,87 @@ class Blockchain(object):
         content = f'{index}{hash_of_previous_block}{transactions} {nonce}'.encode()
         content_hash = hashlib.sha256 (content).hexdigest()
         return content_hash [: len(self.difficulty_target)] == self.difficulty_target
+    
+    def append_block(self, hash_of_previous_block, nonce):
+        block = {
+            'index' : len(self.chain),
+            'timestamp' : time(),
+            'transaction' : self.current_transaction,
+            'nonce' : nonce,
+            'hash_of_previous_block' : hash_of_previous_block
+        }
+
+        self.current_transaction = []
+        self.chain.append(block)
+        return block
+
+    def add_transaction(self, sender, recipient, amount):
+        self.current_transaction.append({
+            'amount' : amount,
+            'recipient' : recipient,
+            'sender' : sender
+        })
+        return self.last_block['index'] + 1
+    
+    @property
+    def last_block(self):
+        return self.chain[-1]
+    
+app = Flask(__name__)
+
+node_identifier = str(uuid4()).replace('-', "")
+
+blockchain = Blockchain()
+
+# === Routes ===
+@app.route('/blockchain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain' : blockchain.chain,
+        'length' : len(blockchain.chain)
+    }
+
+    return jsonify(response), 200
+
+@app.route('/mine', methods=['GET'])
+def mine_block():
+    blockchain.add_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1
+    )
+
+    last_block_hash = blockchain.hash_block(blockchain.last_block)
+
+    index = len(blockchain.chain)
+    nonce = blockchain.proof_of_work(index, last_block_hash, blockchain.current_transaction)
+
+    block = blockchain.append_block(nonce, last_block_hash)
+    response = {
+        'message' : 'A New Block Has Been Discovered',
+        'index' : block['index'],
+        'hash_of_previous_block' : block['hash_of_previous_block'],
+        'nonce' : block['nonce'],
+        'transaction' : block['transaction']
+    }
+
+    return jsonify(response), 200
+
+@app.route('/transaction/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+    required_fields = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required_fields):
+        return ('Missing Fields', 400)
+    
+    index = blockchain.add_transaction(
+        values['sender'],
+        values['recipient'],
+        values['amount']
+    )
+
+    response = {'message': f'Transaction will be added to block {index}'}
+    return (jsonify(response), 201)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(sys.argv[1]))
